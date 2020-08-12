c6ce689c2b406258cbcfb1811b4ba0e7f73f4384
LoboAnimae <andyquan12@gmail.com>
2020-08-11 22:40:38 -0600

    Added decorations

---
+++ decorations.py
@@ -0,0 +1,31 @@
+import time
+import sys
+import random
+
+ERROR_COLOR = '\u001b[41;1m'
+RESET_COLOR = '\u001b[0m'
+SUCCESS_COLOR = '\u001b[43;1m'
+
+
+def loadingpercentage(initnum: int, endnum: int):
+    """Writes the percentage done.
+
+    Args:
+        initnum (int): Where the percentage should start
+        endnum (int): Where the percentage should end
+    """
+    for i in range(initnum, endnum):
+        time.sleep(0.01)
+        j = 0
+        stri = ''
+        while j <= i:
+            stri += '|'
+            j += 1
+        sys.stdout.write(u"\u001b[1000D" + str(i+1) + "%" + str(stri))
+        if i%20 == random.randint(0, 20):
+            time.sleep(random.randint(0, 2))
+        sys.stdout.flush()
+
+
+# loadingpercentage(0, 50)
+# loadingpercentage(51, 100)
